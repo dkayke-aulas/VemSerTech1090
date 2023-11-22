a663571ba99a1d2e3ce7e0e14d57c588589424f1
Dannyel Kayke <dannyel_kayke@hotmail.com>
2023-11-22 20:12:39 -0300

    add aula05 C e D

--- typecript/src/aula05-c/index.ts
+++ typecript/src/aula05-c/index.ts
@@ -1,8 +1,29 @@
 import { tela1, tela2 } from "./Tela";
 
+class Banana {
+
+}
+
+interface Contrato1 {
+  dataDeInicio: Date
+}
+
+interface Contrato2 {
+  nadar(banana: string, melancia: number): void
+}
+
+class AtorGlobal implements Contrato2 {
+  nadar(): void {
+  }
+  // dataDeInicio: Date
+}
+
+const atorGlobal = new AtorGlobal()
+atorGlobal.nadar()
+
 const Aula05 = () => {
-  tela1();
-  tela2();
+  // tela1();
+  // tela2();
 };
 
 export { Aula05 };
--- typecript/src/aula05-d/index.js
+++ typecript/src/aula05-d/index.js
@@ -1,8 +1,43 @@
-const button = document.querySelector('button')
-const input = document.querySelector('#banana')
+const buttonAdicionar = document.querySelector('#add')
+const buttonEditar = document.querySelector('#edit')
+const inputTexto = document.querySelector('#txt')
+const inputIndice = document.querySelector('#indice')
 
-const callback = () => {
-    console.log('clicou', input.value)
+class Postagem {
+    data
+    dataEdicao
+    editado
+    texto 
+
+    constructor(texto) {
+        this.data = new Date()
+        this.dataEdicao = null
+        this.editado = false
+        this.texto = texto
+    }
+
+    editar(novoTexto) {
+        this.texto = novoTexto
+        this.editado = true
+        this.dataEdicao = new Date()
+    }
 }
 
-button.addEventListener('click', callback)
+const localStoragePostagens = localStorage.getItem('postagem') ?? '[]'
+const postagens = JSON.parse(localStoragePostagens)
+
+const callbackAdicionar = () => {
+    const postagem = new Postagem(inputTexto.value)
+    postagens.push(postagem)
+    localStorage.setItem('postagem', JSON.stringify(postagens))
+    inputTexto.value = ''
+}
+
+const callbackEditar = () => {
+    const postagem = postagens[inputIndice.value]
+    postagem.editar(inputTexto.value)
+    localStorage.setItem('postagem', JSON.stringify(postagens))
+}
+
+buttonAdicionar.addEventListener('click', callbackAdicionar)
+buttonEditar.addEventListener('click', callbackEditar)
